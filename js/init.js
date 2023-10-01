2e7410ac09ff3c9e3258bd75da0d2adf2947c31f
Sebastian Perez <98673108+sebas-developer@users.noreply.github.com>
2023-10-01 19:21:10 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -53,6 +53,10 @@ botonDropdown.addEventListener("click", () => {
 // Obtén una referencia al botón
 const toggleButton = document.getElementById("toggle");
 
+if (!(localStorage.getItem("theme") == "light") || !(localStorage.getItem("theme") == "dark")) {
+  localStorage.setItem("theme", "light");
+}
+
 // Obtén todos los elementos con la clase "light" y "light2"
 const elementsToToggle = document.querySelectorAll(".light, .light2");
 
