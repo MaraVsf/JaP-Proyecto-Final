a8328e8cf460416226f01b6334068911e0c6f3d9
marielaveron <maravsf@gmail.com>
2023-11-08 08:20:18 -0300

    Validaciones

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementById("email").value=localStorage.getItem("username");
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("email").value = localStorage.getItem("username");
 });
+
+// Validación con Bootstrap
+(function () {
+  "use strict";
+
+  var forms = document.querySelectorAll(".needs-validation");
+
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
