0a03fc04d22eabb2160ad4e6d985bf7cdbda84b9
sebas-developer <sebastianperez8872@gmail.com>
2023-11-08 08:40:08 -0300

    agregando funcionalidad para perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,57 @@
-document.addEventListener("DOMContentLoaded", () => {
+// Archivos editados: js/my-profile.js, js/index.js, my-profile.html. — Integrar en siguiente pull request.
+
+const botonGuardar = document.querySelector("#guardarPerfil");
+
+document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("email").value = localStorage.getItem("username");
+
+  loadData();
 });
+
+botonGuardar.addEventListener("click", () => {
+  let inputNombre = document.querySelector("#nombre").value;
+  let inputSegNombre = document.querySelector("#segNombre").value;
+  let inputApellido = document.querySelector("#apellido").value;
+  let inputSegApellido = document.querySelector("#segApellido").value;
+  let inputEmail = document.querySelector("#email").value;
+  let inputTelefono = document.querySelector("#telefono").value;
+
+  if (inputNombre != "") {
+    localStorage.setItem("nombre", inputNombre);
+  }
+  if (inputSegNombre != "") {
+    localStorage.setItem("segNombre", inputSegNombre);
+  }
+  if (inputApellido != "") {
+    localStorage.setItem("apellido", inputApellido);
+  }
+  if (inputSegApellido != "") {
+    localStorage.setItem("segApellido", inputSegApellido);
+  }
+  if (inputEmail != "") {
+    localStorage.setItem("email", inputEmail);
+  }
+  if (inputTelefono != "") {
+    localStorage.setItem("telefono", inputTelefono);
+  }
+
+  loadData();
+});
+
+function loadData() {
+  const localStorageItems = [
+    "nombre",
+    "segNombre",
+    "apellido",
+    "segApellido",
+    "email",
+    "telefono",
+  ];
+
+  localStorageItems.forEach((item) => {
+    document.getElementById(`${item}`).value = localStorage.getItem(item);
+  });
+}
 
 // Validación con Bootstrap
 (function () {
