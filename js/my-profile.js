58a8bb01693b5f1d397813e3e10e89d4c8a457a4
fafoddm <fabriddm@gmail.com>
2023-11-12 10:37:36 -0300

    foto de perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -75,3 +75,14 @@ function loadData() {
     );
   });
 })();
+document.getElementById("imagenPerfil").addEventListener("change", function(event) {
+  let file = event.target.files[0];
+  let reader = new FileReader();
+
+  reader.onload = function(e) {
+    let imageData = e.target.result;
+    localStorage.setItem("profileImage", imageData);
+  };
+
+  reader.readAsDataURL(file);
+});
