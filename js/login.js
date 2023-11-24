563baa635de87ef46ad2593f3ae1cf20d05ec111
eumery <eumeryward@gmail.com>
2023-11-24 12:56:16 -0300

    tarea2-3

--- js/login.js
+++ js/login.js
@@ -1,16 +1,44 @@
+
+
 document.getElementById("loginForm").addEventListener("submit", (e) => {
   e.preventDefault();
 
   const username = document.querySelector('[name="nombre"]').value;
   const password = document.querySelector('[name="contraseña"]').value;
   const email = document.querySelector('[name="email"]').value;
-
-  if (username !== "" && password !== "" && password.length >= 6) {
-    localStorage.setItem("loggedIn", "true");
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    location.href = "index.html";
-  } else {
-    alert("Usuario invalido, ingresa una contaseña con 6 o más caracteres.");
-  }
+  const userData = {
+    username: username,
+    password: password
+  };
+  
+  fetch("http://localhost:3000/login", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    // Verificar si data.token existe y su propiedad value es igual a true
+    if (data.token) {
+      localStorage.setItem("username", username);
+      localStorage.setItem("email", email);
+      localStorage.setItem("loggedIn", "true"); 
+      location.href = "index.html";
+    } else {
+      alert("Credenciales incorrectas");
+    }
+  })
+  .catch(error => {
+    console.error('Error en la solicitud fetch:', error);
+    // Manejar errores, por ejemplo, mostrar un mensaje al usuario
+  });
+  
 });
--- servidor/app.js
+++ servidor/app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const path = require("path");
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const jwt = require("jsonwebtoken");
+const key = "clave";
 
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -55,3 +57,138 @@ app.post("/cart", (req, res)=>{
         res.send("El archivo se guardo con exito!");
     });
 });
+
+
+//LOGIN USUARIO
+
+
+app.use(express.json());
+
+// Endpoint para autenticar al usuario y generar el token JWT
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Verificar las credenciales del usuario (ejemplo simple)
+  if (username === 'usuario' && password === '1234') {
+    // Payload del token
+    const payload = {
+      username: username,
+      // Puedes agregar más información al token si lo necesitas
+    };
+
+    // Generar el token JWT con una firma secreta (clave secreta)
+    const token = jwt.sign(payload, 'claveSecreta', { expiresIn: '1h' }); // Puedes ajustar el tiempo de expiración
+
+    // Enviar el token como respuesta al cliente
+    res.json({token});
+  } else {
+    res.status(401).json({ message: 'Credenciales inválidas' });
+  }
+});
+
+// Otro middleware y rutas...
+
+
+/* app.use(bodyParser.json()); // <== ES UN MIDDLEWARE
+// Parsea el cuerpo de las solicitudes en formato JSON.
+
+//Nos inventamos datos de usuarios
+let usuarios = [
+    {id: 1, usuario:"juan", clave: "clavejuan"}, //la clave TIENE QUE ENCRIPTARSE ANTES de guardarse en la bbdd
+    {id: 2, usuario: "maria", clave: "clavemaria"},
+    {id: 3, usuario: "bruno", clave:"clavebruno"}
+];
+
+
+
+//Creo el middleware que va a chequear el login
+function mwLogin(req, res, next){
+const { usuario, clave} = req.body;
+
+//¿Hay usuario y clave?
+if (!usuario || !clave){
+    return res.status(401).json({msj: 'Falta usuario o clave'});
+}
+
+//¿Existe el usuario y la clave  en mi lista?
+const usuarioExiste = usuarios.find(user=> user.usuario ===usuario && user.clave===clave );
+if (!usuarioExiste){
+    return res.status(401).json({msj: 'Usuario o clave no válidos'});
+}
+//Genero el Token JWT
+const token = jwt.sign({userId: usuario.id, username:usuarioExiste.usuario},key,{expiresIn: Math.floor(Date.now()/1000)+10});
+console.log(token);
+res.token = token;
+next();
+}
+
+//Protejo la ruta con el token
+
+app.post('/login', mwLogin, (req, res)=>{
+
+    res.send('Te dejo ingresar y te di un token. ');
+});
+
+app.get('/lista-protegida',(req,res)=>{
+    const token=req.headers['authorization'];
+    //Si no hay token
+    if(!token){
+        return res.status(401).json({msj: 'No tenés permiso!!!'});
+    }
+    //Si hay, quiero ver si no está vencido.
+    jwt.verify(token,key,(err, decoded)=>{
+        if(err){
+            return res.status(401).json({msj: 'Token vencido valor, andá a comprar otro.'});
+        }
+    //Si llego acá es porque está tooooodo bien.
+   
+    res.json({msj: 'Acceso permitido', usuario: decoded});    
+    });
+});
+
+
+  // Endpoint POST /login
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Buscar al usuario en la lista de usuarios
+    const usuario = usuarios.find((u) => u.username === username && u.password === password);
+  
+    if (!usuario) {
+      return res.status(401).json({ mensaje: 'Credenciales incorrectas' });
+    }
+  
+    // Generar un token con jsonwebtoken
+    const token = jwt.sign({ userId: usuario.id, username: usuario.username }, 'secreto', { expiresIn: '1h' });
+  
+    // Devolver el token como respuesta
+    res.json({ token });
+  });
+ 
+  // Endpoint POST /signup para crear un nuevo usuario
+  app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+  
+    // Verificar si el usuario ya existe
+    const usuarioExistente = usuarios.find((u) => u.username === username);
+  
+    if (usuarioExistente) {
+      return res.status(400).json({ mensaje: 'El usuario ya existe' });
+    }
+  
+    // Crear un nuevo usuario
+    const nuevoUsuario = {
+      id: usuarios.length + 1,
+      username,
+      password,
+    };
+  
+    // Agregar el nuevo usuario a la lista
+    usuarios.push(nuevoUsuario);
+  
+    // Generar un token con jsonwebtoken para el nuevo usuario
+    const token = jwt.sign({ userId: nuevoUsuario.id, username: nuevoUsuario.username }, 'secreto', { expiresIn: '1h' });
+  
+    // Devolver el token como respuesta
+    res.json({ token });
+  }); */
